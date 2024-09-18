93ec3ecfde98b18bc5f84b9168decb0065eb6169
Emma Andersson <anderssons-emma@hotmail.com>
2024-09-18 13:43:55 +0200

    Password done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NextButton from "./NextButton";
 import User from "./User";
+import Password from "./Password";
 
 const Login = () => {
   return(
     <div>
       Login
       <User />
+      <Password />
       <NextButton />
     </div>
   )
