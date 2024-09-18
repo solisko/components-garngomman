19d1fc7d250a62de08beccde02664128d631cc1a
Nellie Gränd <nellie.grand@hotmail.com>
2024-09-18 16:35:34 +0200

    added buttons to profil.jsx

--- src/components/Profil/Profil.jsx
+++ src/components/Profil/Profil.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Profil = () => {
-  return <div>Profil</div>;
+  return (
+    <div>
+      <h2>Profil</h2>
+      <button>Kontoinställningar</button>
+      <button>Personinställningar</button>
+      <button>Support</button>
+      <button>GDPR</button>
+      <button>Medlemskap</button>
+      <button>Preferenser</button>
+    </div>
+  );
 };
 
 export default Profil;
