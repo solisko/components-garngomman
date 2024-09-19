72c4e6a714e7781a3f0edaf2db2237509edb041e
Louise Eriksson <louiseriksson.92@gmail.com>
2024-09-19 09:45:24 +0200

    uppdated StashFlikar styling

--- src/components/Stash/StashFlikar.jsx
+++ src/components/Stash/StashFlikar.jsx
@@ -5,10 +5,10 @@ const StashFlikar = () => {
   return (
     <div className={styles.StashFlikarContainer}>
       <div>
-        <button>Flik 1</button>
-        <button>Flik 2</button>
-        <button>Flik 3</button>
-        <button>Flik 4</button>
+        <button className={styles.tabBtn}>Flik 1</button>
+        <button className={styles.tabBtn}>Flik 2</button>
+        <button className={styles.tabBtn}>Flik 3</button>
+        <button className={styles.tabBtn}>Flik 4</button>
       </div>
     </div>
   );
