f7e247d6f9397354ff36d4338c2284f9a813ccaf
Nellie Gränd <nellie.grand@hotmail.com>
2024-09-23 19:53:33 +0200

    added inputs, button and styling to AccountSettings-component

--- src/components/Profil/accountSettings/accountSettings.jsx
+++ src/components/Profil/accountSettings/accountSettings.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import styles from "./AccountSettings.module.css";
 
 const AccountSettings = () => {
-  return <div>Kontoinställningar</div>;
+  return (
+    <div className={styles.accountcontainer}>
+      <h2>Kontoinställningar</h2>
+      <form className={styles.accountform}>
+        <input
+          className={styles.accountinput}
+          type="text"
+          placeholder="Användarnamn"
+        />
+        <input
+          className={styles.accountinput}
+          type="text"
+          placeholder="Lösenord"
+        />
+        <input
+          className={styles.accountinput}
+          type="text"
+          placeholder="E-postadress"
+        />
+        <button className={styles.accountbtn}>Spara</button>
+      </form>
+    </div>
+  );
 };
 
 export default AccountSettings;
