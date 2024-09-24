cc493f7b91bf50de96bc6d1c069115ae43e30ff3
Emma Andersson <anderssons-emma@hotmail.com>
2024-09-24 14:23:17 +0200

    First logocomp done

---
+++ src/components/Logos/Logos.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo1 from "./Logo1";
+
+const Logos = () => {
+    return(
+        <div className="logosContainer">
+            <Logo1 />
+        </div>
+    )
+}
+
+export default Logos;
