f1cd831fb754b95473886adf9d6e8e44fd0b42f6
Louise Eriksson <louiseriksson.92@gmail.com>
2024-09-24 11:44:23 +0200

    updated accesoryList

--- src/components/Stash/AccessoryStash/AccesoryList.jsx
+++ src/components/Stash/AccessoryStash/AccesoryList.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import accesoryData from "../accesoryData.json";
+import styles from "../Stash.module.css";
+import Bgarn from "../../../Assets/Bgarn.png";
 
 const AccesoryList = () => {
-  return <div>AccesoryList</div>;
+  const [accesoryList, setAccesoryList] = useState(accesoryData);
+
+  return (
+    <div>
+      <ul className={styles.listContainer}>
+        {accesoryList.map((accesory) => (
+          <li className={styles.stashList} key={accesory.id}>
+            <div>
+              <img className={styles.stashListImg} src={Bgarn} alt="" />
+              <p className={styles.stashListName}>{accesory.name}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 export default AccesoryList;
