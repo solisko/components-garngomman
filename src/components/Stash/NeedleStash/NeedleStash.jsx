397168e2f20bee975c7408b21a8be2677556ac9b
Louise Eriksson <louiseriksson.92@gmail.com>
2024-09-24 11:27:31 +0200

    needleStash done

--- src/components/Stash/NeedleStash/NeedleStash.jsx
+++ src/components/Stash/NeedleStash/NeedleStash.jsx
@@ -1,6 +1,20 @@
 import React from "react";
+import styles from "../Stash.module.css";
+import StashFlikar from "../StashFlikar";
+import StashBtn from "../StashBtn";
+import StashSearch from "../StashSearch";
+import NeedleList from "./NeedleList";
 
 const NeedleStash = () => {
-  return <div>NeedleStash</div>;
+  return (
+    <div>
+      <StashFlikar />
+      <div className={styles.stashContainer}>
+        <StashSearch />
+        <StashBtn />
+        <NeedleList />
+      </div>
+    </div>
+  );
 };
 export default NeedleStash;
