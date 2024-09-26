365911ab54171144aaa7243a6e75711d7afe870a
Nellie Gränd <nellie.grand@hotmail.com>
2024-09-26 19:47:11 +0200

    added button to Membership component

---
+++ src/components/Profil/Membership/MembershipButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Membership.module.css";
+
+const MembershipButton = () => {
+  return (
+    <div>
+      <button className={styles.MembershipButton}>Spara</button>
+    </div>
+  );
+};
+
+export default MembershipButton;
---
+++ src/components/Profil/Membership/MembershipButton.stories.jsx
@@ -0,0 +1,8 @@
+import MembershipButton from "./MembershipButton";
+
+export default {
+  title: "COMPONENTS/Profil/Membership/MembershipButton",
+  component: MembershipButton,
+};
+
+export const Default = {};
