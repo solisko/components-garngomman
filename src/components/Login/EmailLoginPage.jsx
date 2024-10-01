ad6cf436b8144ac7c9af0ec5fa5bacb7fa0dc1a2
Emma Andersson <anderssons-emma@hotmail.com>
2024-10-01 13:35:24 +0200

    started with loginEmail

---
+++ src/components/Login/EmailLoginPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NextButton from "./NextButton";
+import Header from "../Header/Header";
+
+const EmailLoginPage = () => {
+    return(
+        <div>
+            Email
+            <Header />
+            <NextButton />
+        </div>
+    )
+}
+
+export default EmailLoginPage;
