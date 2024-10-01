c7d236135bf4a9884f63f5cbbb1cc4cc2b0a35ad
Louise Eriksson <louiseriksson.92@gmail.com>
2024-10-01 14:37:22 +0200

    fixed landingpage for when empty yarnList

--- src/components/Stash/YarnStash/YarnList.jsx
+++ src/components/Stash/YarnStash/YarnList.jsx
@@ -1,24 +1,28 @@
-import React, { useState } from "react";
-import yarnData from "../yarndata.json";
+import React from "react";
 import styles from "../Stash.module.css";
 import Bgarn from "../../../Assets/Bgarn.png";
 
-const YarnList = () => {
-  const [yarnList, setYarnList] = useState(yarnData);
-
+const YarnList = ({ yarnList }) => {
   return (
     <div className={styles.listWrapper}>
-      <ul className={styles.listContainer}>
-        {yarnList.map((yarn) => (
-          <li className={styles.stashList} key={yarn.id}>
-            <div>
-              <img className={styles.stashListImg} src={Bgarn} alt="" />
-              <p className={styles.stashListName}>{yarn.name}</p>
-              <p>{yarn.weight} gram</p>
-            </div>
-          </li>
-        ))}
-      </ul>
+      {yarnList.length === 0 ? (
+        <p className={styles.emptyMessage}>
+          Du har inga garn i din gömma. <br />
+          klicka på “+” för att börja lägga till.
+        </p>
+      ) : (
+        <ul className={styles.listContainer}>
+          {yarnList.map((yarn) => (
+            <li className={styles.stashList} key={yarn.id}>
+              <div>
+                <img className={styles.stashListImg} src={Bgarn} alt="" />
+                <p className={styles.stashListName}>{yarn.name}</p>
+                <p>{yarn.weight} gram</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
--- src/components/Stash/YarnStash/YarnList.stories.jsx
+++ src/components/Stash/YarnStash/YarnList.stories.jsx
@@ -1,8 +1,12 @@
+import React from "react";
 import YarnList from "./YarnList";
+import yarnData from "../yarndata.json";
 
 export default {
   title: "COMPONENTS/Stash/YarnStash/YarnList",
   component: YarnList,
 };
 
-export const Default = {};
+export const Default = () => <YarnList yarnList={yarnData} />;
+
+export const EmptyList = () => <YarnList yarnList={[]} />;
