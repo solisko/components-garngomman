4a780ebebe8e9179b8ed4523fdd76c3315c5d86b
Louise Eriksson <louiseriksson.92@gmail.com>
2024-10-01 14:47:02 +0200

    fixed landingpage for empty needleList

--- src/components/Stash/NeedleStash/NeedleList.jsx
+++ src/components/Stash/NeedleStash/NeedleList.jsx
@@ -1,24 +1,28 @@
-import React, { useState } from "react";
-import needleData from "../needleData.json";
+import React from "react";
 import styles from "../Stash.module.css";
 import Bgarn from "../../../Assets/Bgarn.png";
 
-const NeedleList = () => {
-  const [needleList, setNeedleList] = useState(needleData);
-
+const NeedleList = ({ needleList }) => {
   return (
     <div className={styles.listWrapper}>
-      <ul className={styles.listContainer}>
-        {needleList.map((needle) => (
-          <li className={styles.stashList} key={needle.id}>
-            <div>
-              <img className={styles.stashListImg} src={Bgarn} alt="" />
-              <p className={styles.stashListName}>{needle.name}</p>
-              <p>{needle.diameter} mm</p>
-            </div>
-          </li>
-        ))}
-      </ul>
+      {needleList.length === 0 ? (
+        <p className={styles.emptyMessage}>
+          Du har inga tillbehör i din gömma. <br />
+          klicka på “+” för att börja lägga till.
+        </p>
+      ) : (
+        <ul className={styles.listContainer}>
+          {needleList.map((needle) => (
+            <li className={styles.stashList} key={needle.id}>
+              <div>
+                <img className={styles.stashListImg} src={Bgarn} alt="" />
+                <p className={styles.stashListName}>{needle.name}</p>
+                <p>{needle.diameter} mm</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
