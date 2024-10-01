28493e29cd69689045f1379a8c7a94cd29aa2b5e
Louise Eriksson <louiseriksson.92@gmail.com>
2024-10-01 14:28:25 +0200

    fixed landingpage when emty acessoryList

--- src/components/Stash/AccessoryStash/AccesoryList.jsx
+++ src/components/Stash/AccessoryStash/AccesoryList.jsx
@@ -1,23 +1,27 @@
-import React, { useState } from "react";
-import accesoryData from "../accesoryData.json";
+import React from "react";
 import styles from "../Stash.module.css";
 import Bgarn from "../../../Assets/Bgarn.png";
 
-const AccesoryList = () => {
-  const [accesoryList, setAccesoryList] = useState(accesoryData);
-
+const AccesoryList = ({ accesoryList }) => {
   return (
     <div className={styles.listWrapper}>
-      <ul className={styles.listContainer}>
-        {accesoryList.map((accesory) => (
-          <li className={styles.stashList} key={accesory.id}>
-            <div>
-              <img className={styles.stashListImg} src={Bgarn} alt="" />
-              <p className={styles.stashListName}>{accesory.name}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
+      {accesoryList.length === 0 ? (
+        <p className={styles.emptyMessage}>
+          Du har inga tillbehör i din gömma. <br />
+          klicka på “+” för att börja lägga till.
+        </p>
+      ) : (
+        <ul className={styles.listContainer}>
+          {accesoryList.map((accesory) => (
+            <li className={styles.stashList} key={accesory.id}>
+              <div>
+                <img className={styles.stashListImg} src={Bgarn} alt="" />
+                <p className={styles.stashListName}>{accesory.name}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
