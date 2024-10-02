13ff2e9b122b7ebc767a00785b8d55045ad51f24
rana safi <ranaa_safi@hotmail.com>
2024-10-02 13:31:31 +0200

    serach

--- src/components/Community/Community.jsx
+++ src/components/Community/Community.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import SearchBar from './SearchBar';
-import SearchBtn from './SearchBtn';
 import Inspo from './Inspo';
 import styles from './Community.module.css';
 import AvatarUser from './AvatarUser';
@@ -12,18 +11,13 @@ const avatarUrl = "https://i.pravatar.cc";
 const Community = () => {
     return (
         <div>
-
-        <AvatarUser username={username} avatarUrl={avatarUrl} />
-
             <div className={styles['search-container']}>
                 <div className={styles['search-bar']}>
                     <SearchBar />
                 </div>
-
-                <div className={styles['search-btn']}>
-                    <SearchBtn />
-                </div>
             </div>
+            
+        <AvatarUser username={username} avatarUrl={avatarUrl} />
 
             <p className={styles.titleInspo}>Inspiration Feed</p>
 
@@ -34,7 +28,6 @@ const Community = () => {
             <div className={styles['loadMore-btn']}>
                     <LoadMoreBtn />
                 </div>
-
         </div>
     );
 };
