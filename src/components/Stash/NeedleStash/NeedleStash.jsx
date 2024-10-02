ba45448fe600e065d5528f1c051f80d14935a831
Louise Eriksson <louiseriksson.92@gmail.com>
2024-10-02 14:13:46 +0200

    trying to set props to stash component

--- src/components/Stash/NeedleStash/NeedleStash.jsx
+++ src/components/Stash/NeedleStash/NeedleStash.jsx
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
 import needleData from "../needleData.json";
 import styles from "../Stash.module.css";
-import StashFlikar from "../StashFlikar";
 import StashBtn from "../StashBtn";
 import StashSearch from "../StashSearch";
 import NeedleList from "./NeedleList";
+import StashTab from "../StashTab";
 
 const NeedleStash = () => {
   const [needleList, setNeedleList] = useState(needleData);
 
   return (
     <div>
-      <StashFlikar />
+      <div className={styles.tabBtnContainer}>
+        <StashTab title="Tab 1" />
+        <StashTab title="Tab 2" />
+        <StashTab title="Tab 3" />
+        <StashTab title="Tab 4" />
+      </div>
       <div className={styles.stashContainer}>
         <div className={styles.stashHead}>
           <StashSearch />
