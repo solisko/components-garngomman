814649917a3841bcfc0f896e7097313e4ec68eb1
Louise Eriksson <louiseriksson.92@gmail.com>
2024-10-03 10:40:56 +0200

    try to fyxe style on tabBtn

--- src/components/Stash/Stash.jsx
+++ src/components/Stash/Stash.jsx
@@ -8,13 +8,13 @@ import needleData from "./needleData.json";
 import accesoryData from "./accesoryData.json";
 import StashTab from "./StashTab";
 
-const Stash = ({ title1, title2, title3, title4 }) => {
+const Stash = ({ title1, title2, title3, title4, style }) => {
   const stashList = [...yarnData, ...needleData, ...accesoryData];
 
   return (
     <div>
       <div className={styles.tabBtnContainer}>
-        <StashTab title={title1} />
+        <StashTab className={style} title={title1} />
         <StashTab title={title2} />
         <StashTab title={title3} />
         <StashTab title={title4} />
