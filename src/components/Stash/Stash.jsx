50e82a95f92a476b17c43a103c79b4218d5e1d2c
Louise Eriksson <louiseriksson.92@gmail.com>
2024-10-04 14:04:44 +0200

    updated props for stashTab

--- src/components/Stash/Stash.jsx
+++ src/components/Stash/Stash.jsx
@@ -8,13 +8,16 @@ import needleData from "./needleData.json";
 import accesoryData from "./accesoryData.json";
 import StashTab from "./StashTab";
 
-const Stash = ({ title1, title2, title3, title4 }) => {
+const Stash = ({ title1, title2, title3, title4, btnClassName }) => {
   const stashList = [...yarnData, ...needleData, ...accesoryData];
 
   return (
     <div>
       <div className={styles.tabBtnContainer}>
-        <StashTab title={title1} />
+        <StashTab
+          className={`${styles.tabBtn} ${btnClassName}`}
+          title={title1}
+        />
         <StashTab title={title2} />
         <StashTab title={title3} />
         <StashTab title={title4} />
