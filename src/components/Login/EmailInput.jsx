b3ee55ba107ff750a54a9772c9bbba07fa959e28
Emma Andersson <anderssons-emma@hotmail.com>
2024-10-04 09:26:32 +0200

    mobileversion & state

--- src/components/Login/EmailInput.jsx
+++ src/components/Login/EmailInput.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import styles from './Login.module.css';
 
-const EmailInput = () => {
+const EmailInput = ({ setEmailInput }) => {
+    const onEmailInputComplete = (e) => {
+        setEmailInput(e.target.value);
+    }
     return(
         <div className={styles.emailLoginContainer}>
-            <input placeholder='Emailadress' type="email" className={styles.emailInput} />
+            <input 
+                placeholder='Emailadress' 
+                type="email" 
+                className={styles.emailInput}
+                onBlur={onEmailInputComplete} />
         </div>
     )
 }
