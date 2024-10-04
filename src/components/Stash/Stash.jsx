409cd31f95eef3de966859dcfd9d4dd1a31b4728
Louise Eriksson <louiseriksson.92@gmail.com>
2024-10-04 08:48:33 +0200

    small change stash

--- src/components/Stash/Stash.jsx
+++ src/components/Stash/Stash.jsx
@@ -8,13 +8,13 @@ import needleData from "./needleData.json";
 import accesoryData from "./accesoryData.json";
 import StashTab from "./StashTab";
 
-const Stash = ({ title1, title2, title3, title4, style }) => {
+const Stash = ({ title1, title2, title3, title4 }) => {
   const stashList = [...yarnData, ...needleData, ...accesoryData];
 
   return (
     <div>
       <div className={styles.tabBtnContainer}>
-        <StashTab className={style} title={title1} />
+        <StashTab title={title1} />
         <StashTab title={title2} />
         <StashTab title={title3} />
         <StashTab title={title4} />
