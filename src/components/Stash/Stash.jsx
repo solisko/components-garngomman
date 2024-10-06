2f0781791be6895ad468a665374cfc4cc8e1413f
Louise Eriksson <louiseriksson.92@gmail.com>
2024-10-06 22:07:23 +0200

    updated stash and removed list from stashcomponent

--- src/components/Stash/Stash.jsx
+++ src/components/Stash/Stash.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import styles from "./Stash.module.css";
 import StashSearch from "./StashSearch";
 import StashBtn from "./StashBtn";
-import StashList from "./StashList";
-import yarnData from "./yarndata.json";
-import needleData from "./needleData.json";
-import accesoryData from "./accesoryData.json";
+// import StashList from "./StashList";
+// import yarnData from "./yarndata.json";
+// import needleData from "./needleData.json";
+// import accesoryData from "./accesoryData.json";
 import StashTab from "./StashTab";
 
 const Stash = ({ title1, title2, title3, title4, btnClassName }) => {
-  const stashList = [...yarnData, ...needleData, ...accesoryData];
+  // const stashList = [...yarnData, ...needleData, ...accesoryData];
 
   return (
     <div>
@@ -27,7 +27,7 @@ const Stash = ({ title1, title2, title3, title4, btnClassName }) => {
           <StashSearch />
           <StashBtn />
         </div>
-        <StashList stashList={stashList} />
+        {/* <StashList stashList={stashList} /> */}
       </div>
     </div>
   );
