8b3085de68efbca399c6b72695734addab9b625f
Louise Eriksson <louiseriksson.92@gmail.com>
2024-10-06 22:27:27 +0200

    Changed my stash component to be more changeble in the frontend later

--- src/components/Stash/Stash.jsx
+++ src/components/Stash/Stash.jsx
@@ -8,24 +8,44 @@ import StashBtn from "./StashBtn";
 // import accesoryData from "./accesoryData.json";
 import StashTab from "./StashTab";
 
-const Stash = ({ title1, title2, title3, title4, btnClassName }) => {
+const Stash = ({
+  title1,
+  title2,
+  title3,
+  title4,
+  btnClassName,
+  newProjectBtnclassName,
+  stashContainerclassName,
+  tabBtnContainerclassName,
+}) => {
   // const stashList = [...yarnData, ...needleData, ...accesoryData];
 
   return (
     <div>
-      <div className={styles.tabBtnContainer}>
+      <div className={`${styles.tabBtnContainer} ${tabBtnContainerclassName}`}>
         <StashTab
           className={`${styles.tabBtn} ${btnClassName}`}
           title={title1}
         />
-        <StashTab title={title2} />
-        <StashTab title={title3} />
-        <StashTab title={title4} />
+        <StashTab
+          className={`${styles.tabBtn} ${btnClassName}`}
+          title={title2}
+        />
+        <StashTab
+          className={`${styles.tabBtn} ${btnClassName}`}
+          title={title3}
+        />
+        <StashTab
+          className={`${styles.tabBtn} ${btnClassName}`}
+          title={title4}
+        />
       </div>
-      <div className={styles.stashContainer}>
+      <div className={`${styles.stashContainer} ${stashContainerclassName}`}>
         <div className={styles.stashHead}>
           <StashSearch />
-          <StashBtn />
+          <StashBtn
+            className={`${styles.newProjectBtn} ${newProjectBtnclassName}`}
+          />
         </div>
         {/* <StashList stashList={stashList} /> */}
       </div>
