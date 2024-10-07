d271d8b91f42121300c61d8684ed32091604e7c9
Emma Andersson <anderssons-emma@hotmail.com>
2024-10-07 15:22:23 +0200

    Login export done

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,5 +1,6 @@
 import Start from "./Start";
 import EmailLoginPage from "./EmailLoginPage";
 import EmailPasswordPage from "./EmailPasswordPage";
+import Login from "./Login"
 
-export { Start, EmailLoginPage, EmailPasswordPage };
+export { Start, EmailLoginPage, EmailPasswordPage, Login };
